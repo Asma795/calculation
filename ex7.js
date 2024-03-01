804996d16806632d861101a90de59b498f4c42cf
Asma Shahid <asmashahid795@gmail.com>
2024-03-02 01:15:12 +0500

    Add files via upload
    i made  simple calculations

---
+++ ex7.js
@@ -0,0 +1,8 @@
+var sum = 5 + 3;
+var multiply = 4 * 2;
+var divide = 16 / 2;
+var subtract = 10 - 2;
+console.log(sum);
+console.log(multiply);
+console.log(divide);
+console.log(subtract);
---
+++ ex7.ts
@@ -0,0 +1,11 @@
+let sum = 5 + 3;
+let multiply = 4 * 2;
+let divide = 16 / 2;
+let subtract = 10 - 2;
+
+
+
+console.log(sum);
+console.log(multiply);
+console.log(divide);
+console.log(subtract);
